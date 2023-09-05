7e8423f61ffc967b54495ae6a0ba418c2d43294c
Lucas Maccarini <lucas.maccarini95@gmail.com>
2023-09-05 17:45:53 -0300

    changed embedding type: openai embeddings

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 import jwt
 from fastapi import FastAPI, Header, Request, Depends, Response, Form
-import json
-import fnmatch
-import os
 from typing import Optional
-from fastapi.responses import StreamingResponse
 import pandas as pd
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from fastapi import UploadFile, File, Body
 import io
-#from config import settings
-
 
 from bertopic.dimensionality import BaseDimensionalityReduction
 import nltk
@@ -27,8 +21,14 @@ import functools
 from nltk import word_tokenize
 from sklearn.cluster import KMeans
 from transformers.pipelines import pipeline
+from config import settings
+import openai
+from bertopic.backend import OpenAIBackend
 
-#JWT_SECRET = settings.m1d_api_token
+OPENAI_KEY = settings.openai_api_key
+openai.api_key = OPENAI_KEY
+
+JWT_SECRET = settings.m1d_api_token
 JWT_ALGORITHM = "HS256"
 
 app = FastAPI()
@@ -52,7 +52,7 @@ relevant = {"no", "ni", "sin", "nada", "sí", "estad", "fuera"}
 UMAP_N_COMPONENTS = 5
 DBSCAN_MIN_CLUSTER_SIZE =  30
 N_NEIGHBORS = 5 
-embedding_model = "paraphrase-multilingual-MiniLM-L12-v2"
+embedding_model = OpenAIBackend("text-embedding-ada-002")
 umap_model = UMAP(n_neighbors=N_NEIGHBORS, n_components=UMAP_N_COMPONENTS, min_dist=0.0, metric='cosine')
 hdbscan_model = HDBSCAN(min_cluster_size=DBSCAN_MIN_CLUSTER_SIZE, min_samples=15, metric='euclidean', cluster_selection_method='eom', prediction_data=True)
 vectorizer_model = CountVectorizer(ngram_range = (2,3), stop_words=STOPWORDS)
@@ -78,30 +78,36 @@ def healthz():
 @app.post("/clusterize")
 async def clusterize(
     csv_file: Optional[UploadFile] = Form(None),
+    authorization: str = Header(None)
 ):
-    df = pd.read_csv(csv_file.file)
-    docs = df["temática"].tolist()
-    topic_model = BERTopic(
-    embedding_model=embedding_model,
-    umap_model=umap_model,
-    hdbscan_model=hdbscan_model,
-    vectorizer_model=vectorizer_model,
-    ctfidf_model=ctfidf_model,
-    calculate_probabilities=True,
-    nr_topics = "auto",
-    )
-    topics, probs = topic_model.fit_transform(docs)
-    freq = topic_model.get_topic_info()
-    print(freq.to_json())
-    return freq.to_json()
+    try:
+        decoded = secure(authorization)
+    except:
+        return "Acceso Denegado."
+    try:
+        df = pd.read_csv(csv_file.file)
+        docs = df["tematica"].tolist()
+        topic_model = BERTopic(
+        embedding_model=embedding_model,
+        umap_model=umap_model,
+        hdbscan_model=hdbscan_model,
+        vectorizer_model=vectorizer_model,
+        ctfidf_model=ctfidf_model,
+        calculate_probabilities=True,
+        #nr_topics = "auto",
+        )
+        topics, probs = topic_model.fit_transform(docs)
+        freq = topic_model.get_topic_info()
+        print(freq.to_json())
+        return freq.to_json()
+    except Exception as e:
+        print(e)
+        return "Se ha producido un error en la ejecución."
 
 
-    
 
+def secure(token):
+    decoded_token = jwt.decode(token, JWT_SECRET, algorithms=JWT_ALGORITHM)
 
-#def secure(token):
- #   decoded_token = jwt.decode(token, JWT_SECRET, algorithms=JWT_ALGORITHM)
-
-    
- #   return decoded_token
+    return decoded_token
 
